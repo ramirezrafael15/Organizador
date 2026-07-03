d088dba580659869c07fc020f082c6b3e19fa708
OsunaRamírezRafael <osunaramirezrafael@gmail.com>
2026-07-03 13:04:51 +0200

    Update cache name from 'tareas-v3' to 'tareas-v4'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tareas-v3';
+const CACHE_NAME = 'tareas-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
